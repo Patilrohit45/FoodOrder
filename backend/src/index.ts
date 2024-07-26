fd70dc2f2df45437167c20e12d9369c4c132cf58
Patilrohit45 <patilrohit.official@gmail.com>
2024-07-26 12:34:20 +0530

    Database conenction added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,20 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
 
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to Database !"));
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test",async(req,res)=>{
-    res.json({message:"Hello!"});
+app.get("/test", async (req, res) => {
+  res.json({ message: "Hello!" });
 });
 
-app.listen(3000,()=>{
-    console.log("server started on localhost:3000");
+app.listen(3000, () => {
+  console.log("server started on localhost:3000");
 });
