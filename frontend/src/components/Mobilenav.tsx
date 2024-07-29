2d2ffbcbca4124e4e99ee9691b2a8109bb3ae1d6
Patilrohit45 <patilrohit.official@gmail.com>
2024-07-29 16:14:31 +0530

    Login and Logout functionality added.

--- frontend/src/components/Mobilenav.tsx
+++ frontend/src/components/Mobilenav.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
-import  { Sheet,SheetContent,SheetDescription,SheetTitle,SheetTrigger } from './ui/sheet';
-import { Menu } from 'lucide-react';
-import { Separator } from '@radix-ui/react-separator';
-import { Button } from './ui/button';
- "./ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { CircleUserRound, Menu } from "lucide-react";
+import { Separator } from "@radix-ui/react-separator";
+import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+import MobileNavLinks from "./MobileNavLinks";
+("./ui/sheet");
 
 export default function Mobilenav() {
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0();
+
   return (
     <Sheet>
-        <SheetTrigger>
-                <Menu className='text-orange-500'/>
-        </SheetTrigger>
-        <SheetContent className='space-y-3'>
-            <SheetTitle>
-                <span>Welcome to OrderEats!</span>
-            </SheetTitle>
-            <Separator/>
-            <SheetDescription className='flex'>
-                <Button className='flex-1 font-bold bg-orange-500'>Log In</Button>
-            </SheetDescription>
-        </SheetContent>
+      <SheetTrigger>
+        <Menu className="text-orange-500" />
+      </SheetTrigger>
+      <SheetContent className="space-y-3">
+        <SheetTitle>
+          {isAuthenticated ? (
+            <span className="flex tems-center font-bold gap-2">
+              <CircleUserRound className="text-orange-500" />
+              {user?.email}
+            </span>
+          ) : (
+            <span>Welcome to OrderEats!</span>
+          )}
+        </SheetTitle>
+        <Separator />
+        <SheetDescription className="flex flex-col gap-4">
+          {isAuthenticated ? (
+            <MobileNavLinks />
+          ) : (
+            <Button
+              className="flex-1 font-bold 
+          bg-orange-500"
+          onClick={()=>loginWithRedirect()}
+            >
+              Log In
+            </Button>
+          )}
+        </SheetDescription>
+      </SheetContent>
     </Sheet>
-  )
+  );
 }
